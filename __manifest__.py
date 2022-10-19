d04ab316bd34d1ad731b4a1df1da3ef0c336a567
divacodeuse <k.ciss@isepdiamniadio.edu.sn>
2022-10-19 15:09:35 +0000

    second

--- __manifest__.py
+++ __manifest__.py
@@ -25,8 +25,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/bulletin_views.xml',
+        'views/extrait_views.xml',
+        'views/acte_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
